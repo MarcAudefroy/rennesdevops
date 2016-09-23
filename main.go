c0943f778b98a10fb38d2bb2b9a82578310ef66c
Marc <marc.audefroy@ovh.fr>
2016-09-24 00:56:51 +0200

    add cobra

--- main.go
+++ main.go
@@ -5,54 +5,57 @@ import (
 	"os"
 
 	"github.com/ovh/tat"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-// taturl, username / password of tat engine
-var (
-	taturl   string
-	username string
-	password string
-)
-
-/*
-Usage:
- go get -u github.com/ovh/tat
- build && ./mycli-minimal -url=http://url-tat-engine -username=<tatUsername> -password=<tatPassword> /Internal/your/topic your message
-*/
-
 func main() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	viper.SetConfigType("json")
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	var mainCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
 	}
+	mainCmd.AddCommand(statsCmd)
 
-	taturl = viper.GetString("taturl")
-	username = viper.GetString("username")
-	password = viper.GetString("password")
+	mainCmd.Execute()
+}
 
-	client, err := tat.NewClient(tat.Options{
-		URL:      taturl,
-		Username: username,
-		Password: password,
-		Referer:  "main.v0",
-	})
+var statsCmd = &cobra.Command{
+	Use:   "stats",
+	Short: "Stats Instances",
+	Run: func(cmd *cobra.Command, args []string) {
 
-	if err != nil {
-		fmt.Printf("Error while create new Tat Client: %s\n", err)
-		os.Exit(1)
-	}
+		viper.SetConfigName("config")
+		viper.AddConfigPath(".")
+		viper.SetConfigType("json")
 
-	stats, err := client.StatsInstance()
+		err := viper.ReadInConfig()
+		if err != nil {
+			panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		}
 
-	if err != nil {
-		fmt.Printf("Error:%s\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("instances: %s\n", stats)
+		taturl := viper.GetString("taturl")
+		username := viper.GetString("username")
+		password := viper.GetString("password")
 
+		client, err := tat.NewClient(tat.Options{
+			URL:      taturl,
+			Username: username,
+			Password: password,
+			Referer:  "main.v0",
+		})
+
+		if err != nil {
+			fmt.Printf("Error while create new Tat Client: %s\n", err)
+			os.Exit(1)
+		}
+
+		stats, err := client.StatsInstance()
+
+		if err != nil {
+			fmt.Printf("Error:%s\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("instances: %s\n", stats)
+	},
 }
